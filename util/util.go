85665bfb1f8e4ceab08dd68fb3a95d7610875147
Hedzr Yeh <hedzrz@gmail.com>
2020-10-10 16:35:09 +0800

    delete unused go generate

--- util/util.go
+++ util/util.go
@@ -4,4 +4,4 @@
 
 package util
 
-//go:generate python ../generate-flag-types util -i ../flag-types.json -o flag_generated.go
+//go: generate python ../generate-flag-types util -i ../flag-types.json -o flag_generated.go
